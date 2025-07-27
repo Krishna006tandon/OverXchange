c842551cce56858c45bdba2e0cd7133a1f8ebdb2
Om Bhudake <ombhudke@gmail.com>
2025-07-27 17:55:43 +0530

    Supplier logistics

--- backend/Vendor_app.py
+++ backend/Vendor_app.py
@@ -209,7 +209,7 @@ def create_transaction(current_user):
         'supplier_logistic_charges': float(data.get('supplier_logistic_charges', 0)),
         'created_at': datetime.utcnow()
     }
-    
+    #
     result = transactions.insert_one(transaction)
     transaction['_id'] = str(result.inserted_id)
     
